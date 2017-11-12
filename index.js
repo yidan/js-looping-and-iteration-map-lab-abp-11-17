0d99a8faa952d8ebf6c540506bd116a00576a555
Yidan Wang <wangyidan621@gmail.com>
2017-11-12 17:49:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function nameToAttributes(drivers){
 
 function attributesToPhrase(drivers){
   return drivers.map(function(driver){
-    return `${driver[name]} is from ${driver[hometown]}`
+    return `${driver.name} is from ${driver.hometown}`
   })
 }
