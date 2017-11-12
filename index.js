d5501832e83934a061faa775061f5f31388cafcc
Yidan Wang <wangyidan621@gmail.com>
2017-11-12 17:47:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,9 @@ function nameToAttributes(drivers){
     return {firstName:name[0],lastName:name[1]}
   })
 }
+
+function attributesToPhrase(drivers){
+  return drivers.map(function(driver){
+    return `${driver[name]} is from ${driver[hometown]}`
+  })
+}
