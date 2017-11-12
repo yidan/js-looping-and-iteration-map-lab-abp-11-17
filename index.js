7cbc8ee5bd06bebe41ab7a92cc27d2c28ddfa1e9
Yidan Wang <wangyidan621@gmail.com>
2017-11-12 17:43:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ function lowerCaseDrivers(drivers){
     return driver.toLowerCase()
   })
 }
-
-
-
+function nameToAttributes(drivers){
+  return drivers.map(function(driver){
+    let name = driver.split(" ")
+    return {firstName:name[0],lastName:name[1]}
+  })
+}
